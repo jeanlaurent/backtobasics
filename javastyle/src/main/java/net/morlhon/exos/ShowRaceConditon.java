28a7f315cad41740feec01e97cc1f0a9e163cc17
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-09-05 16:19:07 +0200

    Sheldon Cooper presents FUN WITH THREADS

---
+++ javastyle/src/main/java/net/morlhon/exos/ShowRaceConditon.java
@@ -0,0 +1,95 @@
+package net.morlhon.exos;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ShowRaceConditon {
+
+    interface Counter {
+
+        int inc();
+
+        int get();
+
+    }
+
+    class SynchronizedCounter implements Counter {
+        private int count = 0;
+
+        public int inc() {
+            synchronized (this) {
+                return ++count;
+            }
+        }
+
+        public int get() {
+            return count;
+        }
+
+    }
+
+    class NaiveCounter implements Counter {
+        private int count = 0;
+
+        public int inc() {
+            int newCount = ++count;
+            return newCount;
+        }
+
+        public int get() {
+            return count;
+        }
+    }
+
+    class CounterWithLock implements Counter {
+        private Lock lock = new ReentrantLock();
+        private int count = 0;
+
+        public int inc() {
+            lock.lock();
+            int newCount = ++count;
+            lock.unlock();
+            return newCount;
+        }
+
+        public int get() {
+            return count;
+        }
+    }
+
+    public void go() throws InterruptedException {
+        incrementCounter(new CounterWithLock(), 1000);
+        incrementCounter(new NaiveCounter(), 1000);
+        incrementCounter(new SynchronizedCounter(), 1000);
+
+    }
+
+    private void incrementCounter(Counter counter, int numberOfTime) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < numberOfTime; i++) executorService.execute(new CounterThread(counter));
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        System.out.println(counter.getClass().getSimpleName() + " -> " + counter.get());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new ShowRaceConditon().go();
+
+    }
+
+    public static class CounterThread extends Thread {
+        private final Counter counter;
+
+        public CounterThread(Counter counter) {
+            this.counter = counter;
+        }
+
+        public void run() {
+            counter.inc();
+        }
+    }
+
+}
---
+++ javastyle/src/main/java/net/morlhon/exos/ThreadFun.java
@@ -0,0 +1,57 @@
+package net.morlhon.exos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Date;
+
+public class ThreadFun {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        long start = new Date().getTime();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new GetSerpodileRunnable(),"thread-" + i).start();
+        }
+        long end = new Date().getTime();
+        System.out.println("End time: " + ((end - start)) + "ms");
+    }
+
+    public static String curl(String url) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+        final StringBuilder builder = new StringBuilder();
+        bufferedReader.lines().forEach(line -> builder.append(line + "\n"));
+        return builder.toString();
+    }
+
+    public static class GetSerpodileRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            long start = new Date().getTime();
+            try {
+                curl("http://serpodile.com");
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            long end = new Date().getTime();
+            System.out.println( Thread.currentThread().getName() + " > " + ((end - start)) + "ms");
+        }
+    }
+
+    public static class GetSerpodileThread extends Thread {
+        @Override
+        public void run() {
+            long start = new Date().getTime();
+            try {
+                curl("http://serpodile.com");
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            long end = new Date().getTime();
+            System.out.println( getName() + " > " + ((end - start)) + "ms");
+        }
+    }
+
+}
