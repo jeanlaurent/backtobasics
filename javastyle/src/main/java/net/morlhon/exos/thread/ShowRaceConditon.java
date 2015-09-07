8e0d9353e7b125934741f3301e652b8ddd0a5f5c
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-09-07 10:57:46 +0200

    Move thread stuff around

--- javastyle/src/main/java/net/morlhon/exos/thread/ShowRaceConditon.java
+++ javastyle/src/main/java/net/morlhon/exos/thread/ShowRaceConditon.java
@@ -1,4 +1,4 @@
-package net.morlhon.exos;
+package net.morlhon.exos.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
--- javastyle/src/main/java/net/morlhon/exos/thread/ThreadFun.java
+++ javastyle/src/main/java/net/morlhon/exos/thread/ThreadFun.java
@@ -1,4 +1,4 @@
-package net.morlhon.exos;
+package net.morlhon.exos.thread;
 
 import java.io.BufferedReader;
 import java.io.IOException;
